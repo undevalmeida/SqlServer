73b50287041a5e5411b317d94817b74dad687753
André <andrealmeidaa00@outlook.com>
2022-07-29 22:54:23 -0300

    Separando cada script

--- SQLQuery12.sql
+++ SQLQuery12.sql
@@ -1,22 +1,3 @@
-USE SUCOS_VENDAS /* SETANDO PARA BANDO DE DADOS SUCO_VENDAS*/
-
-/*INSERIR CONTE�DO*/
-INSERT INTO PRODUTOS(
-	cod_produto,
-	nome_produto,
-	embalagem,
-	tamanho,
-	sabor,
-	preco_lista
-)
-VALUES(
-		'100058'
-	,	'Chot'
-	,	'Lata'
-	,	'250ml'
-	,	'Morango'
-	,	8.90
-)
 
 /*ALTERANDO CONTE�DO DA TABELA*/
 UPDATE PRODUTOS SET
